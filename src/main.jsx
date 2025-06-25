563c809869a38969907955d9323082951302b99c
Deepan.karthegeyen31 <Deepan.karthegeyen@infoservices.com>
2025-06-25 14:19:38 +0530

    main jsx changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,7 +10,7 @@ import Contact from './pages/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
